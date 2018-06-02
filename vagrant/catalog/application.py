1a89ce55daa182027c0ba7119c78d363804b0736
Henrique Truta <henriquet@spotify.com>
2018-06-03 01:36:47 +0200

    Create and delete

--- vagrant/catalog/application.py
+++ vagrant/catalog/application.py
@@ -1,6 +1,6 @@
 #!/usr/local/bin/python
 # -*- coding: utf-8 -*-
-from flask import Flask, render_template
+from flask import Flask, redirect, request, render_template, url_for
 from db.db_handler import Database
 app = Flask(__name__)
 
@@ -8,32 +8,60 @@ db = Database()
 
 @app.route("/")
 def home():
-    cats = db.list_categories()
-    return render_template('home.html', categories=cats)
+    categories = db.list_categories()
+    return render_template('home.html', categories=categories)
 
-@app.route("/catalog/<int:category_id>/", methods=["POST"])
+
+@app.route("/item/new", methods=["POST", "GET"])
 def add_item():
-    return "Adding Item"
+    if request.method == 'POST':
+        category = request.form['category']
+        db.insert_item(request.form['title'],
+                       request.form['description'],
+                       category)
+        return redirect(url_for('list_items', category_id=category))
+    else:
+        categories = db.list_categories()
+        return render_template('new_item.html', categories=categories)
+
+
+@app.route("/catalog/<int:item_id>/details", methods=["GET"])
+def get_item(item_id):
+    item = db.get_item(item_id)
+    return render_template('item_description.html', item=item)
 
 
 @app.route("/catalog/<int:category_id>/list", methods=["GET"])
 def list_items(category_id):
     items = db.list_items(category_id)
     category_name = db.get_category_name(category_id)
-    print len(items)
-    print category_name
     return render_template('items.html', items=items,
                            category_name=category_name)
 
 
-@app.route("/catalog/<int:category_id>/<int:item_id>", methods=["DELETE"])
-def delete_item():
-    return "Deleting Item"
+@app.route("/item/<int:item_id>/delete", methods=["POST", "GET"])
+def delete_item(item_id):
+    item = db.get_item(item_id)
+    if request.method == 'POST':
+        db.delete_item(item_id)
+        return redirect(url_for('list_items', category_id=item.categoryId))
+    else:
+        return render_template('item_delete.html', item=item)
 
 
 @app.route("/catalog/login", methods=["POST"])
 def login():
     return "Login"
 
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, use_reloader=True, use_debugger=True)
+
+
+# TODO:
+# Edit item
+# Login
+# Check if the user created
+# Latest items
+# HTML
+# CSS
--- vagrant/catalog/db/db_handler.py
+++ vagrant/catalog/db/db_handler.py
@@ -12,8 +12,26 @@ class Database():
     def list_items(self, category):
         return self.session.query(Item).filter_by(categoryId=category).all()
 
+    def insert_item(self, title, description, category):
+        item = Item(title=title, description=description,
+                    category=self._get_category(category))
+        self.session.add(item)
+        self.session.commit()
+
     def list_categories(self):
         return self.session.query(Category).all()
 
+    def delete_item(self, item_id):
+        item = self.session.query(Item).filter_by(id=item_id).one()
+        print "item", item
+        self.session.delete(item)
+        self.session.commit()
+
     def get_category_name(self, category_id):
         return self.session.query(Category).filter_by(id=category_id).one().title
+
+    def _get_category(self, category_id):
+        return self.session.query(Category).filter_by(id=category_id).one()
+
+    def get_item(self, item_id):
+        return self.session.query(Item).filter_by(id=item_id).one()
