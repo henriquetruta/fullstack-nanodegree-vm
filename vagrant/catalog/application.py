2403844c04ba92150151b536cac752870f2ecb0a
Henrique Truta <henriquet@spotify.com>
2018-06-09 19:21:53 +0200

    Minor fix

--- vagrant/catalog/application.py
+++ vagrant/catalog/application.py
@@ -105,7 +105,7 @@ def login():
     state = ''.join(random.choice(string.ascii_uppercase + string.digits)
                     for x in xrange(32))
     login_session['state'] = state
-    return render_template('login.html', state=state)
+    return render_template('login.html', STATE=state)
 
 
 @app.route('/gconnect', methods=['POST'])
